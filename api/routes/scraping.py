ee5e32f9e24b90dd4bcf7a41235ad5397cb20f78
Carlos Santos <cadu.vs@hotmail.com>
2025-07-24 16:37:10 -0300

    feat: scrapping trigger

--- api/routes/scraping.py
+++ api/routes/scraping.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, Depends
-
+from fastapi import APIRouter, Depends, HTTPException
+from scraping import download_catalogue_data
 from api.core.auth import require_admin
 
 router = APIRouter()
@@ -7,4 +7,11 @@ router = APIRouter()
 
 @router.post("/trigger")
 def start_scrapping(_: str = Depends(require_admin)):
-    return {"Status": "Searching..."}
+    try:
+        download_catalogue_data()
+        return {"message": "Scraping executed successfully."}
+    except Exception:
+        raise HTTPException(
+            status_code=500,
+            detail="An error occurred while executing the scraping.",
+        )
