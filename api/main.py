050347083f26adbea22c3e75cd2390635a671d6e
Andre-Rafael <andrerafael2000@gmail.com>
2025-08-03 13:26:36 -0300

    roda o scrapping no inicio da aplicacao

--- api/main.py
+++ api/main.py
@@ -3,9 +3,11 @@ from fastapi import FastAPI
 
 import api.routes as routes
 from api.logger.setup import setup_logging
+from scraping.books import download_catalogue_data
 
 Path('data').mkdir(exist_ok=True)
 setup_logging("data/logs.db")
+download_catalogue_data()
 
 app = FastAPI(title="Books API", version="1.0")
 
